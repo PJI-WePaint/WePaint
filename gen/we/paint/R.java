466c361f75b3684c3c9480a4976aadb4ebf18b7f
Damien Level <level.damien@gmail.com>
2012-03-21 17:25:45 +0100

    Ajout du design

--- gen/we/paint/R.java
+++ gen/we/paint/R.java
@@ -11,13 +11,57 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int add=0x7f020000;
+        public static final int circle=0x7f020001;
+        public static final int color=0x7f020002;
+        public static final int config=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int logo=0x7f020005;
+        public static final int micro=0x7f020006;
+        public static final int move=0x7f020007;
+        public static final int square=0x7f020008;
+        public static final int triangle=0x7f020009;
+    }
+    public static final class id {
+        public static final int accelerometerPic=0x7f050013;
+        public static final int accelerometerText=0x7f050015;
+        public static final int addDesc=0x7f050000;
+        public static final int addPic=0x7f050007;
+        public static final int addText=0x7f050009;
+        public static final int circlePic=0x7f050002;
+        public static final int colorPic=0x7f050008;
+        public static final int colorText=0x7f05000a;
+        public static final int compassPic=0x7f050012;
+        public static final int compassText=0x7f050014;
+        public static final int configPic=0x7f05000f;
+        public static final int configText=0x7f050010;
+        public static final int logoPic=0x7f050005;
+        public static final int micrPic=0x7f05000c;
+        public static final int microText=0x7f05000e;
+        public static final int moveDesc=0x7f050011;
+        public static final int movePic=0x7f05000b;
+        public static final int moveText=0x7f05000d;
+        public static final int squarePic=0x7f050003;
+        public static final int tableAdd=0x7f050001;
+        public static final int tableHome=0x7f050006;
+        public static final int trianglePic=0x7f050004;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int add=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int move=0x7f030002;
     }
     public static final class string {
+        public static final int accelerometer=0x7f04000a;
+        public static final int add=0x7f040002;
+        public static final int addDesc=0x7f040007;
         public static final int app_name=0x7f040001;
+        public static final int color=0x7f040003;
+        public static final int compass=0x7f040009;
+        public static final int configuration=0x7f040006;
         public static final int hello=0x7f040000;
+        public static final int move=0x7f040004;
+        public static final int moveDesc=0x7f040008;
+        public static final int vocal=0x7f040005;
     }
 }
